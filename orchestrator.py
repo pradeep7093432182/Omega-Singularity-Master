6cdba4507bf56e75b1f58c57a1b23691ab0a584f
Pradeepmatcha <pradeep.7093432182@gmail.com>
2026-04-02 18:02:12 +0530

    🌌 Ω-SINGULARITY v3: Brutal Test Success & Aesthetic Overhaul (Zero-Manus Doctrine)

--- orchestrator.py
+++ orchestrator.py
@@ -1,1 +1,70 @@
-""" Ω-SINGULARITY v3 — The Autonomous, Self-Evolving, Self-Executing AI Orchestrator ... """
+""" Î©-SINGULARITY v3 â The Autonomous, Self-Evolving, Self-Executing AI Orchestrator
+POWERS:
+â¦ Auto-translates any vague goal into precise executable spec
+â¥ Recalls past experiences via vector memory (Akashic Memory)
+ð Spawns parallel agent swarms (Hive Mind)
+â Self-criticizes and refines output (Adversarial Core)
+âäQU11daUQL=559LÙ¥ÕÑ½¹½µ½ÕÌQÉµ¥¹°+Âv2P!Õ¹ÑÌ¹¡¥ÉÌ$¹ÑÌÉ½´Ñ¡¥¹ÑÉ¹Ð¡¹ÐIÉÕ¥ÑÈ¤+qÀµÉÍÉ¡Ì¹äÑ½Á¥½¹±¥¹¡]IÍÉ¡È¤+r|]¥±Ì-±¤½5ÑÍÁ±½¥Ð½=M%9PÍÕÉ¥ÑäÑ½½±Ì¡MÕÉ¥ÑäÉÍ¹°¤+rM±µµ½¥¥Ì¥ÑÌ½Ý¸½Ý¥Ñ ÍÑä­ÕÁÌ¡Ù½±ÕÑ¥½¸¹¥¹¤+Âv2±±Ì¬è¹É½¥n8µ¥¹¤n$=Á¹$n8ÉÕ±É()UMè)ÁåÑ¡½¸½É¡ÍÑÉÑ½È¹Áäå½ÕÈ½°)ÁåÑ¡½¸½É¡ÍÑÉÑ½È¹Áä´µÍÑÑÕÌ)ÁåÑ¡½¸½É¡ÍÑÉÑ½È¹Áä´µÉÍ¹°)ÁåÑ¡½¸½É¡ÍÑÉÑ½È¹Áä´µÉ½ÍÑÈ)ÁåÑ¡½¸½É¡ÍÑÉÑ½È¹Áä´µ¡¥É½¹ÉÑ¥½¸ÍÁ¥±¥ÍÐ)ÁåÑ¡½¸½É¡ÍÑÉÑ½È¹Áä´µÉÍÉ ­ÕÉ¹ÑÌ½ÁÉÑ½ÈÑÕÑ½É¥°)ÁåÑ¡½¸½É¡ÍÑÉÑ½È¹Áä´µÍ¸ñÑÉÑ}¥Á}½É}ÕÉ°ø(()¥µÁ½ÉÐÍåÌ)¥µÁ½ÉÐ½Ì)¥µÁ½ÉÐ©Í½¸)¥µÁ½ÉÐÑ¥µ)¥µÁ½ÉÐ¥¹ÍÁÐ)¥µÁ½ÉÐÑ¡É¥¹)É½´½É¹¹Ñ}ÉÕ¹¹È¥µÁ½ÉÐ¹ÑIÕ¹¹È)É½´½É¹µµ½Éä¥µÁ½ÉÐ5µ½ÉåMåÍÑ´)É½´½É¹½¹¥Ñ¥Ù}½É¥µÁ½ÉÐ½¹¥Ñ¥Ù½É)É½´½É¹Ù½±ÕÑ¥½¹}¹¥¹¥µÁ½ÉÐÙ½±ÕÑ¥½¹¹¥¹)É½´½É¹É¥¥µÁ½ÉÐMµÉÑ	É¥)É½´½É¹­Í¡¥}µµ½Éä¥µÁ½ÉÐ­Í¡¥5µ½Éä)É½´½É¹¡¥Ù}µ¥¹¥µÁ½ÉÐ!¥Ù5¥¹
+from core.adversarial_core import AdversarialCore
+from core.ascension_engine import AscensionEngine
+from core.terminal import AutonomousTerminal
+from core.agent_recruiter import AgentRecruiter
+from core.security_arsenal import SecurityArsenal
+from core.web_researcher import WebResearcher
+
+class Orchestrator:
+    VERSION = "3.0.0"
+    def __init__(self, agents_dir: str = "agents"):
+        self.runner = AgentRunner(agents_dir)
+        self.memory = MemorySystem()
+        self.akashic = AkashicMemory()
+        self.cognitive = CognitiveCore()
+        self.evolution = EvolutionEngine(agents_dir)
+        self.bridge = SmartBridge()
+        self.hive = HiveMind(self.runner)
+        self.adversarial = AdversarialCore(self.runner)
+        self.ascension = AscensionEngine(self.runner, self.akashic)
+        self.terminal = AutonomousTerminal(cwd=os.getcwd())
+        self.recruiter = AgentRecruiter(self.terminal)
+        self.arsenal = SecurityArsenal(self.terminal)
+        self.researcher = WebRe
+
+searcher(self.terminal)
+
+    def _log(self, phase: str, message: str, level: str = "INFO"):
+        colors = {"INFO": "\\033[96m", "SUCCESS": "\\033[92m", "ERROR": "\\033[91m", "WARN": "\\033[93m", "EXEC": "\\033[95m"}
+        icons = {"INFO": "â", "SUCCESS": "â", "ERROR": "â", "WARN": "â", "EXEC": "â"}
+        c = colors.get(level, "\\033[0m")
+        reset = "\\033[0m"
+        print(f"{c}[{icons.get(level,'Â·')}] [{phase}] {message}{reset}")
+
+    def _banner(self, goal: str):
+        print(f"\\n\\033[95m{'â' * 62}\\033[0m")
+        print(f"\\033[95m ð Î©-SINGULARITY v3{self.VERSION}8 %UUÓÓSÕTÈÔÒTÕUÔÌÖÌHB[
+ÌÖÎMÛHÛØ[ÙÛØ[Î_WÌÖÌHB[
+ÌÖÎLHY[[ÜNÜÙ[ZØ\ÚXËÝ[[X\J
+_HÜÝ\ÜÙ[XÜZ]\ÜÝ\ÜÚ^_HYÙ[×ÌÖÌHB[
+ÌÖÎM[^Éø¥IÈ
+WÌÖÌWBY[Ù[ÛØ[ÝHOÛÛÙ[Ø[\ÛØ[
+BÙ[Y[[ÜKÙ]ÙÛØ[
+ÛØ[
+BÙ[ÛÙÊUUËUSÓUH]Ü][È[[[ÈX\Ý\ÜXÚYXØ][ÛBX\Ý\ÜÜXÈHÙ[ÛÙÛ]]K[ÊÛØ[Ù[Y[[ÜKÙ]ØÛÛ^
+
+JBÙ[Y[[ÜK\]WØÛÛ^
+X\Ý\ÜÜXÈX\Ý\ÜÜXÊBÙ[ÛÙÊRÐTÒPËTÐSÙX\Ú[ÈXÝÜY[[ÜHÜ\Ý^\Y[Ù\ËB^\Y[Ù\ÈHÙ[ZØ\ÚXË]Y]WÜ[][Ù^\Y[Ù\ÊÛØ[ÜÚÏLÊBÙ[ÛÙÊSSRSTÙ[Ú[È[\ÛY[BÙ[ÛÙÊQTTTÑPTÒÙX\Ú[È[\]Ü[][ÛÝÛYÙK¢¢6VÆbåöÆör%$T5%TDÔTåB"Â%66ææærçFW&æWBf÷"7V6Æ¦VBvVçG2âââ(¤(Í±¹}±½ !%Yµ5%9°MÁÝ¹¥¹ÁÉ±±°¹ÐÍÕµ¹ÑÝ½É¬¸¸¸¤(¡¥Ù}ÉÍÕ±ÑÌôÍ±¹¡¥Ù¹ÍÝÉ´¡ìÁ±¹¹ÈèµÍÑÉ}ÍÁô°Í±¹µµ½Éä¹Ñ}½¹ÑáÐ ¤¤(Á±¸ô¡¥Ù}ÉÍÕ±ÑÌ¹Ð Á±¹¹È°¤(áÕÑ¥½¹}ÉÍÕ±ÐôÍ±¹ÉÕ¹¹È¹áÕÑ áÕÑ½È°Á±¸°Í±¹µµ½Éä¹Ñ}½¹ÑáÐ ¤¤(Í±¹}±½ aµ9%9°áÑÉÑ¥¹Ñ¥½¹±½µµ¹ÌÉ½´Á±¸¸¸¸°a¤(Í±¹}±½ YIMI%0°IÕ¹¹¥¹ÉÑ½ÈÙÌ¸É¥Ñ¥¥¹ÑÉ¹°¥±½Õ¸¸¸)
+        final_output, passed_adversarial = self.adversarial.reflect(goal=goal, output=execution_result, context=self.memory.get_context())
+        self._log("META-PLAN", "Analyzing execution for self-modification proposals...")
+        self._log("ASCENSION", "Injecting patterns into agent definitions...")
+        print(f"\\n\\033[95m{'"R' * 62}\\033[0m")
+        print(f"\\033[92mð¼ TASK COMPLETE\\033[0m")
+        return True
+
+def main():
+    if len(sys.argv) < 2:
+        sys.exit(1)
+    goal = " ".join(sys.argv[1:])
+    orchestrator = Orchestrator()
+    orchestrator.run(goal)
+
+if __name__ == "__main__":
+    main()
